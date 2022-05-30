89afb8e8e08dec93e392785bfd79ce563c7e5f75
jejecrunch <gkswlgp456@gmail.com>
2022-05-31 04:02:45 +0900

    mod: Hello World 파일 삭제 및 NthWeek 파일 추가로 App 파일 수정

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+  <NthWeek />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NthWeek from './components/NthWeek.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    NthWeek
+}
 }
 </script>
 
 <style>
+body {
+  background-color: #369FAD;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
 }
 </style>
