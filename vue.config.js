aa4431b20d3cefed59b573da3101d259eb456d88
제제jeje <gkswlgp456@gmail.com>
2024-01-20 02:05:08 +0900

    mod: #1 빌드 후 css, js 경로 인식 못하는 이슈

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: process.env.NODE_ENV === "production" ? "/nth-week-today/" : "/",
-    assetsDir: "dist",
-    outputDir: "docs",
+    publicPath: "/nth-week-today",
+    outputDir: "dist",
     pages: {
         index: {
             entry: "src/main.js",
