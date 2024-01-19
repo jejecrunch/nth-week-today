4843b9eb8c304b481fd25368227deaa24c5dec27
제제jeje <gkswlgp456@gmail.com>
2024-01-20 03:29:51 +0900

    test: 액션 테스트

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
     transpileDependencies: true,
     publicPath: "/nth-week-today/",
-    assetsDir: "../assets",
-    outputDir: "./dist",
+    assetsDir: "../dist/assets",
+    outputDir: "dist",
     pages: {
         index: {
             entry: "src/main.js",
